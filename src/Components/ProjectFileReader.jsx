81b1efcc1f5a87b36696ecec712eab5075b078b8
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2024-06-12 15:45:48 +0800

    Input file replacement ui
    See: https://stackoverflow.com/questions/60370260

--- src/Components/ProjectFileReader.jsx
+++ src/Components/ProjectFileReader.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
 
 class ProjectFileReader extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <input type="file" id="projectFilePicker" name="projectFile" accept=".zip" onChange={this.onFileChange} onClick={this.onFileClick}/>
-      </React.Fragment>
-    );
+  constructor(props) {
+    super(props);
+    this.fileRef = React.createRef();
+    this.state = {
+      lastFileSelected: "No file chosen"
+    }
+  }
+
+  onButtonClick = event => {
+    this.fileRef.current.click();
   }
 
   onFileClick = event => {
-    event.target.value = ""
+    event.target.value = "";
   }
 
-  onFileChange = event => { 
+  onFileChange = event => {
+    this.setState({lastFileSelected: event.target.files[0].name});
     this.props.loadProjectFile(event.target.files[0]);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <input type="file" id="projectFilePicker" name="projectFile" accept=".zip" hidden onChange={this.onFileChange} onClick={this.onFileClick} ref={this.fileRef}></input>
+        <button style={{display: "inline", height: "17px", width: "84px"}} onClick={this.onButtonClick}>Choose File</button>
+		<span style={{"font-size": "14px"}}>&nbsp;{this.state.lastFileSelected}</span>
+      </React.Fragment>
+    );
   }
 }
 
