c89357783e6a9f86c3227877418d7646286bfa18
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2024-10-13 14:13:46 +0800

    Automatically zoom out on page overflow

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -18,6 +18,7 @@ class App extends Component {
   componentDidMount()
   {
     console.log("%c203 | Prismatic \nBuild " + preval`module.exports = new Date().toLocaleString("en-US");`, "color: cyan; font-size: 24px;")
+    setTimeout(() => {if (document.body.scrollHeight > document.body.clientHeight) {document.body.style.zoom = "75%"}}, 0)
     this.loadUserConfigPerfences()
     setTimeout((this.initlization).bind(this), 0) //Hacky way to get initlization done after first render
   }
