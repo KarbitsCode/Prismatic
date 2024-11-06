800a6dc704616e0904231534fd98b95370e8d0cd
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2024-11-06 16:34:48 +0800

    Add electron check

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -18,7 +18,7 @@ class App extends Component {
   componentDidMount()
   {
     console.log("%c203 | Prismatic \nBuild " + preval`module.exports = new Date().toLocaleString("en-US");`, "color: cyan; font-size: 24px;")
-    setTimeout(() => {(document.body.scrollHeight > document.body.clientHeight || document.body.scrollWidth > document.body.clientWidth) ? document.body.style.zoom = "75%" : document.body.style.zoom = "100%"}, 0)
+    setTimeout(() => {((document.body.scrollHeight > document.body.clientHeight || document.body.scrollWidth > document.body.clientWidth) ? ((typeof window !== "undefined" && ((typeof window.process === "object" && window.process.type === "renderer") || navigator.userAgent.includes("Electron") || (typeof process !== "undefined" && process.versions && process.versions.electron))) ? document.body.style.zoom = "77%" : document.body.style.zoom = "75%") : document.body.style.zoom = "100%")}, 0)
     this.loadUserConfigPerfences()
     setTimeout((this.initlization).bind(this), 0) //Hacky way to get initlization done after first render
   }
