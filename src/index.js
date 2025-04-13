16173a1a2613c745a15d0b8f584c99e987b0e6ec
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-04-13 16:27:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ ReactDOM.render(<App/>, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (isElectron()) {
-  serviceWorker.unregister();
-} else {
+if (!isElectron()) {
   serviceWorker.register();
+} else {
+  serviceWorker.unregister();
 }
